35b970035b5a802ec759a824399f956b6d250427
sandrotbilisi <atsetsoffc@gmail.com>
2025-10-12 08:54:46 -0700

    react no unespcated entities off

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -21,6 +21,7 @@ const eslintConfig = [
     ],
     rules: {
       "@typescript-eslint/no-explicit-any": "off", // <-- disable the 'any' error
+      "react/no-unescaped-entities": "off",
     },
   },
 ];
